36aa85f6d2635a5a501448d53bda72f2cb02ec6a
toby coleman <tobycoleman3@hotmail.co.uk>
2024-07-02 08:47:11 +0000

    created first function to create a 5 by 5 board

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
 from random import randint
+
+user = []
+user_guesses = []
+comp = []
+
+def make_board(board):
+    """
+    This is to make the initial 5 by 5 board,
+    consisting of 5 rows and 5 lists of " O "
+    """
+    for i in range(5):
+        board.append([" O "]*5)
+    return board
+
